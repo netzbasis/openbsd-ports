56fff3debe9556ceba3f9ea505eed8e14ad12943
Ben <programmer@netzbasis.de>
2015-11-01 08:06:23 +0100

    sync with openbsd-current

--- www/mozilla/mozilla.port.mk
+++ www/mozilla/mozilla.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: mozilla.port.mk,v 1.78 2015/09/24 20:18:12 landry Exp $
+# $OpenBSD: mozilla.port.mk,v 1.80 2015/10/31 15:12:38 landry Exp $
 
 SHARED_ONLY =	Yes
 ONLY_FOR_ARCHS=	amd64 arm i386 powerpc sparc64
@@ -24,7 +24,7 @@ MOZILLA_DIST_VERSION ?=	${MOZILLA_VERSION:C/rc.//}
 HOMEPAGE ?=	http://www.mozilla.org/projects/${MOZILLA_DIST}
 
 .if ${MOZILLA_VERSION:M*rc?}
-MASTER_SITES ?=	https://ftp.mozilla.org/pub/mozilla.org/${MOZILLA_DIST}/nightly/${MOZILLA_DIST_VERSION}-candidates/build${MOZILLA_VERSION:C/.*(.)/\1/}/source/
+MASTER_SITES ?=	https://ftp.mozilla.org/pub/mozilla.org/${MOZILLA_DIST}/candidates/${MOZILLA_DIST_VERSION}-candidates/build${MOZILLA_VERSION:C/.*(.)/\1/}/source/
 # first is the CDN and only has last releases
 # ftp.m.o has all the betas/candidate builds but should only be used as fallback
 .else
@@ -140,7 +140,7 @@ _MOZDIR =	mozilla
 .endif
 
 # needed for PLIST
-MOZILLA_VER =	${MOZILLA_VERSION:C/b[0-9]*$//:C/esr$//:C/rc.$//}
+MOZILLA_VER =	${MOZILLA_VERSION:C/b[0-9]*$//:C/esr//:C/rc.$//}
 SUBST_VARS +=	MOZILLA_PROJECT MOZILLA_VER MOZILLA_VERSION
 
 MAKE_ENV +=	MOZILLA_OFFICIAL=1 \
