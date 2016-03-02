3980b44ccb32638b21eb507871582dbe14826aa4
Ben <programmer@netzbasis.de>
2016-03-02 08:05:35 +0100

    sync with openbsd-current

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.362 2016/02/04 12:34:48 sthen Exp $
+# $OpenBSD: Quirks.pm,v 1.366 2016/03/01 20:44:19 naddy Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -395,6 +395,8 @@ my $stem_extensions = {
 	'apertium-nn-nb' => 'apertium-nno-nob',
 	'weblint' => 'p5-HTML-Lint',
 	'py-fabric' => 'fabric',
+# 6.0
+	'cosmic-sans-neue-mono' => 'fantasque-sans',
 };
 
 # reasons for obsolete packages
@@ -731,6 +733,12 @@ my $obsolete_reason = {
 	'hs-simple-sendfile' => 5,
 	'monadius' => 3,
 	'hs-bimap' => 5,
+	'nicotine' => 3,
+	'toprump' => 3,
+	'fedora_base' => 0,
+	'fedora_cups' => 0,
+	'fedora_gtk+2' => 0,
+	'adom' => 0,
 };
 
 my @msg = (
