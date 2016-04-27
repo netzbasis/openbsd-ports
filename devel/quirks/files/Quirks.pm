3544a13917c46ab077cf57641fb467ee470e2803
Ben <programmer@netzbasis.de>
2016-04-27 08:04:28 +0200

    sync with openbsd-current

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.380 2016/04/21 20:28:26 naddy Exp $
+# $OpenBSD: Quirks.pm,v 1.383 2016/04/26 15:36:23 jasper Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -762,6 +762,10 @@ my $obsolete_reason = {
 	'rplay' => 0,
 	'amsn' => 6,
 	'pebrot' => 6,
+	'alacarte' => 3,
+	'logstash-forwarder' => 3,
+	'py-Xlib' => 3,
+	'pypanel' => 3,
 };
 
 my @msg = (
