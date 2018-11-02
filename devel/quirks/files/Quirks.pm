cf39e0bd210fc1dd163d54709b5ad1985226b001
Ben <programmer@netzbasis.de>
2018-11-02 08:31:35 +0100

    sync with openbsd-current

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.648 2018/10/31 23:01:55 juanfra Exp $
+# $OpenBSD: Quirks.pm,v 1.650 2018/11/01 16:59:18 juanfra Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1068,6 +1068,8 @@ my $obsolete_reason = {
 	'dovecot-antispam' => 12,
 	'py-hgtools' => 3,
 	'py3-hgtools' => 3,
+	'py-hgsubversion' => 3,
+	'py-hgnested' => 5,
 };
 
 # reasons for obsolete packages
