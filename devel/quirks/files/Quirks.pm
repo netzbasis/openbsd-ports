297dc2d91626013257ff07e3a8c8ad2aa306c095
Ben <programmer@netzbasis.de>
2018-12-04 08:29:57 +0100

    sync with openbsd-current

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.666 2018/12/01 12:59:20 martijn Exp $
+# $OpenBSD: Quirks.pm,v 1.668 2018/12/03 20:32:50 danj Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1085,6 +1085,7 @@ my $obsolete_reason = {
 	'p5-Crypt-RSA' => 13,
 	'p5-Crypt-OpenPGP' => 13,
 	'directoryassistant' => 0,
+	'x-pack' => 3,
 };
 
 # reasons for obsolete packages
@@ -1186,8 +1187,8 @@ my $cve = {
 	'audio/flac' => 'flac-<1.3.0p1',
 	'databases/mariadb,-main' => 'mariadb-client-<10.0.36',
 	'databases/mariadb,-server' => 'mariadb-server-<10.0.36',
-	'databases/postgresql,-main' => 'postgresql-client-<10.5',
-	'databases/postgresql,-server' => 'postgresql-server-<10.5',
+	'databases/postgresql,-main' => 'postgresql-client-<10.6',
+	'databases/postgresql,-server' => 'postgresql-server-<10.6',
 	'devel/git,-main' => 'git-<2.19.1',
 	'devel/git,-svn' => 'git-svn-<2.19.1',
 	'devel/git,-x11' => 'git-x11-<2.19.1',
