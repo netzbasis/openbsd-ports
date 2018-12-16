8327b316e5c45d8c92a111110df948f2a47176d2
Ben <programmer@netzbasis.de>
2018-12-16 08:29:35 +0100

    sync with openbsd-current

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.679 2018/12/14 15:33:06 danj Exp $
+# $OpenBSD: Quirks.pm,v 1.681 2018/12/15 20:25:54 danj Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1224,6 +1224,7 @@ my $cve = {
 	'archivers/libmspack' => 'libmspack-<0.8alpha',
 	'archivers/p5-Archive-Zip' => 'p5-Archive-Zip-<1.64',
 	'audio/flac' => 'flac-<1.3.0p1',
+	'databases/sqlite3' => 'sqlite3-<3.25.3',
 	'databases/mariadb,-main' => 'mariadb-client-<10.0.36',
 	'databases/mariadb,-server' => 'mariadb-server-<10.0.36',
 	'databases/postgresql,-main' => 'postgresql-client-<10.6',
@@ -1247,6 +1248,7 @@ my $cve = {
 	'mail/p5-Mail-SpamAssassin' => 'p5-Mail-SpamAssassin-<3.4.2',
 	'mail/roundcubemail' => 'roundcubemail-<1.3.8',
 	'net/curl' => 'curl-<7.62.0',
+	'net/haproxy' => 'haproxy-<1.8.15',
 	'net/icecast' => 'icecast-<2.4.4',
 	'net/isc-bind' => 'isc-bind-<9.11.4pl2',
 	'net/lldpd' => 'lldpd-<0.7.18p0',
