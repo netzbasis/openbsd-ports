517bfbc3c5a47b7ff6e5a410ff552cdfd43cf784
Ben <programmer@netzbasis.de>
2019-02-09 08:33:12 +0100

    sync with openbsd-current

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.714 2019/02/07 20:50:18 phessler Exp $
+# $OpenBSD: Quirks.pm,v 1.716 2019/02/08 15:01:08 sthen Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1270,10 +1270,12 @@ my $cve = {
 	'lang/ruby/2.3,-main' => 'ruby-<2.3.8',
 	'lang/ruby/2.4,-main' => 'ruby->2.4,<2.4.5',
 	'lang/ruby/2.5,-main' => 'ruby->2.5,<2.5.3',
+	'mail/dovecot,-main' => 'dovecot-<2.3.4.1',
 	'mail/exim' => 'exim-<4.83',
 	'mail/p5-Mail-SpamAssassin' => 'p5-Mail-SpamAssassin-<3.4.2',
 	'mail/roundcubemail' => 'roundcubemail-<1.3.8',
 	'math/hdf5' => 'hdf5-<1.8.21',
+	'multimedia/libquicktime' => 'libquicktime-<1.2.4p13',
 	'net/curl' => 'curl-<7.64.0',
 	'net/haproxy' => 'haproxy-<1.8.17',
 	'net/icecast' => 'icecast-<2.4.4',
