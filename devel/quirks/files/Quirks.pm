f51c79e22ae02d643a5607f230a7a820fe40c22c
Ben <programmer@netzbasis.de>
2019-04-28 08:32:20 +0200

    sync with openbsd-current

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.754 2019/04/26 15:10:32 semarie Exp $
+# $OpenBSD: Quirks.pm,v 1.756 2019/04/27 21:26:35 naddy Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -1215,6 +1215,8 @@ my $obsolete_reason = {
 	'py3-async' => 5,
 	'py3-funcsigs' => 5,
 	'rust-doc' => 1,
+	'glib-openssl' => 6,
+	'gcj' => 5,
 };
 
 # reasons for obsolete packages
